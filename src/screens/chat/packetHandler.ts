fb338f04ec86934aaf8a638b5c5daab6ae13ea40
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2022-08-12 13:46:13 +0530

    Fix 1.16 death respawn, tweak chat handling.

--- src/screens/chat/packetHandler.ts
+++ src/screens/chat/packetHandler.ts
@@ -15,6 +15,35 @@ export const sendMessageError = 'Failed to send message to server!'
 export const healthMessage =
   enderChatPrefix + "You're losing health! \u00A7b%prev \u00A7f-> \u00A7c%new"
 
+type HandleError = (
+  addMsg: (text: MinecraftChat) => void,
+  translated: string
+) => (error: unknown) => any
+
+const handleSystemMessage = (
+  packet: Packet,
+  addMessage: (text: MinecraftChat) => void,
+  handleError: HandleError,
+  is1191: boolean
+) => {
+  try {
+    const [chatLength, chatVarIntLength] = readVarInt(packet.data)
+    const chatJson = packet.data
+      .slice(chatVarIntLength, chatVarIntLength + chatLength)
+      .toString('utf8')
+    // TODO: Support position 2 (action bar), true (action bar) and sender for disableChat/blocked players.
+    // TODO-1.19: 3 say command, 4 msg command, 5 team msg command, 6 emote command, 7 tellraw command, also in Player Chat Message.
+    const position = packet.data.readInt8(chatVarIntLength + chatLength)
+    if (!is1191 && (position === 0 || position === 1)) {
+      addMessage(parseValidJson(chatJson))
+    } else if (is1191 && !position) {
+      addMessage(parseValidJson(chatJson))
+    }
+  } catch (e) {
+    handleError(addMessage, parseMessageError)(e)
+  }
+}
+
 export const packetHandler =
   (
     healthRef: React.MutableRefObject<number | null>,
@@ -25,10 +54,7 @@ export const packetHandler =
     joinMessage: string,
     sendJoinMessage: boolean,
     sendSpawnCommand: boolean,
-    handleError: (
-      addMsg: (text: MinecraftChat) => void,
-      translated: string
-    ) => (error: unknown) => any,
+    handleError: HandleError,
     charLimit: number
   ) =>
   (packet: Packet) => {
@@ -58,19 +84,13 @@ export const packetHandler =
       (packet.id === 0x0e && !is117) ||
       (packet.id === 0x0f && is117 && !is119)
     ) {
-      try {
-        const [chatLength, chatVarIntLength] = readVarInt(packet.data)
-        const chatJson = packet.data
-          .slice(chatVarIntLength, chatVarIntLength + chatLength)
-          .toString('utf8')
-        const position = packet.data.readInt8(chatVarIntLength + chatLength)
-        // TODO: Support position 2 (also in 0x5f 1.19 packet) and sender for disableChat/blocked players.
-        if (position === 0 || position === 1) {
-          addMessage(parseValidJson(chatJson))
-        }
-      } catch (e) {
-        handleError(addMessage, parseMessageError)(e)
-      }
+      handleSystemMessage(packet, addMessage, handleError, is1191)
+    } else if (
+      /* System Chat Message (clientbound) */
+      (packet.id === 0x5f && is119 && !is1191) ||
+      (packet.id === 0x62 && is1191)
+    ) {
+      handleSystemMessage(packet, addMessage, handleError, is1191)
     } else if (
       /* Player Chat Message (clientbound) */
       (packet.id === 0x30 && is119 && !is1191) ||
@@ -121,28 +141,6 @@ export const packetHandler =
         handleError(addMessage, parseMessageError)(e)
       }
     } else if (
-      /* System Chat Message (clientbound) */
-      (packet.id === 0x5f && is119 && !is1191) ||
-      (packet.id === 0x62 && is1191)
-    ) {
-      try {
-        const [chatLength, chatVarIntLength] = readVarInt(packet.data)
-        const chatJson = packet.data
-          .slice(chatVarIntLength, chatVarIntLength + chatLength)
-          .toString('utf8')
-        // As of 1.19.1, this is a boolean which says if this is an action bar or not.
-        const position = packet.data.readInt8(chatVarIntLength + chatLength)
-        // TODO-1.19 - 3: say command, 4: msg command, 5: team msg command, 6: emote command, 7: tellraw command
-        // Also in Player Chat Message.
-        if (is119 && (position === 0 || position === 1)) {
-          addMessage(parseValidJson(chatJson))
-        } else if (is1191 && !position) {
-          addMessage(parseValidJson(chatJson))
-        }
-      } catch (e) {
-        handleError(addMessage, parseMessageError)(e)
-      }
-    } else if (
       /* Open Window */
       (packet.id === 0x2e && !is119) ||
       (packet.id === 0x2b && is119 && !is1191) ||
@@ -157,20 +155,30 @@ export const packetHandler =
         .catch(handleError(addMessage, inventoryCloseError))
     } else if (
       /* Death Combat Event */
-      (packet.id === 0x32 && !is117) ||
+      (packet.id === 0x31 && !is117) ||
       (packet.id === 0x35 && is117 && !is119) ||
       (packet.id === 0x33 && is119 && !is1191) ||
       (packet.id === 0x36 && is1191)
     ) {
-      const [, playerIdLen] = readVarInt(packet.data)
-      const offset = playerIdLen + 4 // Entity ID
-      const [chatLen, chatVarIntLength] = readVarInt(packet.data, offset)
-      const jsonOffset = offset + chatVarIntLength
+      let data = packet.data
+      if (!is117) {
+        const [action, actionLen] = readVarInt(data)
+        if (action !== 2) return
+        data = data.slice(actionLen)
+      }
+      data = data.slice(readVarInt(data)[1] + 4) // Remove Player/Entity ID
+      const [chatLen, chatViLength] = readVarInt(data)
       const deathMessage = parseValidJson(
-        packet.data.slice(jsonOffset, jsonOffset + chatLen).toString('utf8')
+        data.slice(chatViLength, chatViLength + chatLen).toString('utf8')
       )
       addMessage(deathRespawnMessage)
-      if (deathMessage?.text || deathMessage?.extra) addMessage(deathMessage)
+      if (
+        (typeof deathMessage === 'string' && deathMessage.trim()) ||
+        deathMessage?.text ||
+        deathMessage?.extra?.length > 0
+      ) {
+        addMessage(deathMessage)
+      }
       // Automatically respawn.
       // LOW-TODO: Should this be manual, or a dialog, like MC?
       connection // Client Status
