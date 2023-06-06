8f1b08a7784091c2d43222017fbb2f08dc79a4fb
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2023-06-07 05:09:11 +0530

    Update to React Native 0.72.0-rc.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   extends: [
-    '@react-native-community',
+    '@react-native',
     'plugin:react/recommended',
     'standard-with-typescript',
     'standard-react',
