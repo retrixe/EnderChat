00cab6982c8b432c2a2520bdd3ec03e1b8b3fbf0
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-01-24 15:41:52 +0530

    Update to React Native 0.77

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,17 +2,13 @@
  * @format
  */
 
-import 'react-native'
 import React from 'react'
+import ReactTestRenderer from 'react-test-renderer'
 import App from '../src/App'
 
-// Note: import explicitly to use the types shipped with jest.
-// eslint-disable-next-line n/no-extraneous-import
-import { it } from '@jest/globals'
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
-
-it('renders correctly', () => {
-  renderer.create(<App />) // eslint-disable-line @typescript-eslint/no-deprecated
+it('renders correctly', async () => {
+  /* eslint-disable @typescript-eslint/no-deprecated */
+  await ReactTestRenderer.act(() => {
+    ReactTestRenderer.create(<App />)
+  })
 })
